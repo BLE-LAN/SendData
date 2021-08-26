c17f121be5a19b9898eb8b74523c9985e9f54118
nyaboron <nyaboronnn@protonmail.com>
2021-08-26 18:39:06 +0200

    Add demo url, update new AdvScanner arguments, add the infinite loop, move some string to global variables.

--- main.py
+++ main.py
@@ -2,9 +2,14 @@ import subprocess
 import argparse
 import requests
 import json
+from time import sleep
 
 
-_URL = ''
+_URL_BASE = 'https://nyaboron.pythonanywhere.com/'
+
+_OUTFILE_NAME = "\\adv_list.txt"
+
+_ADD_URL = 'api/adddevices'
 
 
 def parse_args():
@@ -22,16 +27,21 @@ def parse_args():
 
 
 def run_scanner(scanner_path, scan_time, ouput_path):
-    return subprocess.run([scanner_path, '-s', str(scan_time), '-o', ouput_path])#, stdout=subprocess.DEVNULL)
+    return subprocess.run([scanner_path, '/scantime', str(scan_time), '/fileoutput', ouput_path], stdout=subprocess.DEVNULL)
 
 
 def adv_file_to_json(adv_path):
 
     json_str = '{"devices": ['
 
+    line_count = 0
+
     with open(adv_path) as f:
         for line in f:
+            line_count += 1
             json_str += line[:-1] + ', '
+
+    print('Number of Advertisements: ' + str(line_count))
 
     return json.loads(json_str[:-2] + ']}')
 
@@ -40,19 +50,19 @@ def api_add(jwt, body):
     headers = {
         'Authorization': 'Bearer ' + jwt
     }
-    r = requests.post(_URL, json=body, headers=headers)
+    r = requests.post(_URL_BASE + _ADD_URL, json=body, headers=headers)
 
-    return r.status_code
+    print('Response status code: ' + str(r.status_code) + '\n')
 
 
 if __name__ == '__main__':
     args = parse_args()
 
     path_partitions = args.scanner.rpartition("\\")
-    adv_file_path = path_partitions[0] + "\\ble.txt"
+    adv_file_path = path_partitions[0] + _OUTFILE_NAME
 
-    run_scanner(args.scanner, args.scantime, adv_file_path)
-
-    adv_json = adv_file_to_json(adv_file_path)
-
-    api_add(args.jwt, adv_json)
+    while 1:
+        run_scanner(args.scanner, args.scantime, adv_file_path)
+        adv_json = adv_file_to_json(adv_file_path)
+        api_add(args.jwt, adv_json)
+        sleep(10)
