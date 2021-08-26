3ade151fafba6948500fe3155e82e31e51048463
nyaboron <nyaboronnn@protonmail.com>
2021-08-26 19:01:13 +0200

    Add some exceptions, update some help text, readme.

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
-# SendData
-Little python script to run the AdvScanner and send the ouput to the ListADV web.  
+## SendData
+
+Little Python 3 script to run the [AdvScanner](https://github.com/BLE-LAN/AdvScanner) and 
+send the output to the  [ListADV](https://github.com/BLE-LAN/ListADV) web.
+
+## Dependencies
+
+* requests
+
+## Usage
+
+The AdvScanner out path is the same that de .exe, by default the filename is 'adv_list.txt'.
+
+````
+positional arguments:
+  jwt                   token needed to api auth
+
+optional arguments:
+  -h, --help            show this help message and exit
+  --scanner SCANNER, -s SCANNER
+                        AdvScanner path
+  --scantime SCANTIME, -st SCANTIME
+                        time to scan in seconds
+````
+
+
+
+
+
+
--- main.py
+++ main.py
@@ -4,7 +4,6 @@ import requests
 import json
 from time import sleep
 
-
 _URL_BASE = 'https://nyaboron.pythonanywhere.com/'
 
 _OUTFILE_NAME = "\\adv_list.txt"
@@ -15,28 +14,33 @@ _ADD_URL = 'api/adddevices'
 def parse_args():
     parser = argparse.ArgumentParser(description='Run the ADV Scanner and send the output to the ListADV web')
     parser.add_argument(
-        'jwt', type=str, help="token to ",
+        'jwt', type=str, help="token needed to api auth",
     )
     parser.add_argument(
         '--scanner', '-s', type=str, help="AdvScanner path", default='.\\AdvScanner.exe'
     )
     parser.add_argument(
-        '--scantime', '-st', type=int, help="time to scan in milliseconds", default=(40*1000)
+        '--scantime', '-st', type=int, help="time to scan in seconds", default=(40 * 1000)
     )
     return parser.parse_args()
 
 
 def run_scanner(scanner_path, scan_time, ouput_path):
-    return subprocess.run([scanner_path, '/scantime', str(scan_time), '/fileoutput', ouput_path], stdout=subprocess.DEVNULL)
+    return subprocess.run([scanner_path, '/scantime', str(scan_time), '/fileoutput', ouput_path],
+                          stdout=subprocess.DEVNULL)
 
 
 def adv_file_to_json(adv_path):
-
     json_str = '{"devices": ['
 
     line_count = 0
 
-    with open(adv_path) as f:
+    try:
+        f = open(adv_path)
+    except FileNotFoundError or OSError:
+        raise Exception("could not open adv_list.txt")
+
+    with f:
         for line in f:
             line_count += 1
             json_str += line[:-1] + ', '
@@ -51,6 +55,9 @@ def api_add(jwt, body):
         'Authorization': 'Bearer ' + jwt
     }
     r = requests.post(_URL_BASE + _ADD_URL, json=body, headers=headers)
+
+    if r.status_code != 200:
+        raise Exception(r.text)
 
     print('Response status code: ' + str(r.status_code) + '\n')
 
